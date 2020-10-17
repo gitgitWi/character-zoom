051e8fd60f2a836308c5619047e64b736180a67d
gitgitwi <johnwi@knou.ac.kr>
2020-10-17 20:52:43 +0900

    REFACTOR: some refactoring
    TODO: socket not working

---
+++ public/functions/stream.js
@@ -0,0 +1,16 @@
+// const stream =
+//     connectToNewUser(userId, stream) {
+//         const call = myPeer.call(userId, stream);
+//         const video = document.createElement("video");
+//         call.on("stream", (userVideoStream) => {
+//             addVideoStream(video, userVideoStream);
+//         });
+//         call.on("close", () => {
+//             video.remove();
+//         });
+
+//         peers[userId] = call;
+//     },
+// };
+
+// export default stream;
